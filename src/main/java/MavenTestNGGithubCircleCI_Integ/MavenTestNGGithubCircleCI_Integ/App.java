6603b3c48ec7cb1abbb7afb9da5ef7a84a19644c
LeonelCalderon15 <calderoncodeproject@gmail.com>
2019-06-17 16:31:59 +0800

    Signed-off-by: LeonelCalderon15 <calderoncodeproject@gmail.com>

---
+++ src/main/java/MavenTestNGGithubCircleCI_Integ/MavenTestNGGithubCircleCI_Integ/App.java
@@ -0,0 +1,13 @@
+package MavenTestNGGithubCircleCI_Integ.MavenTestNGGithubCircleCI_Integ;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/TestNG/MavenTestNG_Class.java
@@ -0,0 +1,19 @@
+package TestNG;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class MavenTestNG_Class {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
